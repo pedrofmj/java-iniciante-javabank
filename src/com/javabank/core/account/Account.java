13c50dd44338dbf0d810af5071bae4f4bc2be625
Pedro Ferreira <pedrofmj@gmail.com>
2022-03-19 10:18:37 -0300

    Saving Repository

--- src/com/javabank/core/account/Account.java
+++ src/com/javabank/core/account/Account.java
@@ -1,13 +1,21 @@
 package com.javabank.core.account;
 
+import java.io.IOException;
 import java.util.Date;
+
+import com.google.gson.annotations.Expose;
+import com.javabank.data.json.JsonBankingRepository;
 
 public class Account {
 
+	protected String brand = "0001";
 	protected String name = "";
 	protected String displayName = "";
 	protected double ballance = 0.00;
 	protected AccountStatement statement = null;
+	
+	@Expose(serialize = false, deserialize = false)
+	protected JsonBankingRepository repository = null;
 	
 	public Account(String name, String displayName, double ballance) {
 		setName(name);
@@ -17,6 +25,17 @@ public class Account {
 		if (ballance != 0.00) {
 			addStatementEntry("Initial Ballance", ballance);
 		}
+		try {
+			initialize();
+		} catch (Throwable t) {
+			// NOTHING
+			this.repository = null;
+		}
+	}
+	
+	public void initialize() throws IOException {
+		this.repository = new JsonBankingRepository(String.format("account-%s-%s", this.brand, this.name));
+		this.repository.initialize();
 	}
 	
 	public String getName() {
@@ -43,10 +62,24 @@ public class Account {
 	
 	public void addStatementEntry(Date date, String description, double value) {
 		getStatement().addEntry(date, description, value);
+		try {
+			if (this.repository != null) {
+				this.repository.save();
+			}
+		} catch (Throwable t) {
+			// NOTHING
+		}
 	}
 	
 	public void addStatementEntry(String description, double value) {
 		getStatement().addEntry(description, value);
+		try {
+			if (this.repository != null) {
+				this.repository.save();
+			}
+		} catch (Throwable t) {
+			// NOTHING
+		}
 	}
 	
 }
